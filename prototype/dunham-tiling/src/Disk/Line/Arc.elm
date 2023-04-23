992803b56385e10f8cdbb820eef7f33f2c67625d
Daan van Berkel <daan.van.berkel@alliander.com>
2023-04-23 16:31:41 +0200

    Use an svg path for the hyperbolic circles

--- prototype/dunham-tiling/src/Disk/Line/Arc.elm
+++ prototype/dunham-tiling/src/Disk/Line/Arc.elm
@@ -26,6 +26,12 @@ view a b =
 
         radius =
             Point.norm <| Point.difference a o
+
+        pathDescription =
+            [ Point.use Moveto a
+            , Point.use (Arc radius) b
+            ]
+                |> toString
     in
     Svg.g []
         [ Svg.circle
@@ -34,6 +40,12 @@ view a b =
             , r <| String.fromFloat radius
             , fill "none"
             , stroke "red"
+            ]
+            []
+        , Svg.path
+            [ stroke "blue"
+            , fill "none"
+            , d pathDescription
             ]
             []
         ]
@@ -52,3 +64,28 @@ bisector a b =
             Point.use Tuple.pair <| Point.difference b a
     in
     equation (midpoint a b) -dy dx
+
+
+type alias Path =
+    List PathElement
+
+
+type PathElement
+    = Moveto Float Float
+    | Arc Float Float Float
+
+
+toString : Path -> String
+toString ps =
+    let
+        toS p =
+            case p of
+                Moveto x y ->
+                    "M" ++ (String.join "," <| List.map String.fromFloat [ x, y ])
+
+                Arc radius x y ->
+                    "A" ++ (String.join " " <| List.map String.fromFloat [ radius, radius, 0, 0, 0, x, y ])
+    in
+    ps
+        |> List.map toS
+        |> String.join " "
